b97591251266892f99299fdcf35d11be7d0074e8
Felix Hillebrand <97hilfel@gmail.com>
2021-10-11 11:41:26 +0200

    Cleanup RedditCog
    Rename scrape_scrorebaord to find_posts,
    Rename update_scoreboard to update_posts,
    add update_at guard, no post under min_age gets updated
    drastically change update_posts, speedup proces
    reverse order of messages posted, now in chronological order from oldest to newest

--- my_cogs.py
+++ my_cogs.py
@@ -1,9 +1,13 @@
 from typing import List
 from datetime import datetime, timedelta
 from sqlite3 import Connection
-from models import Config, SubredditState, SubmissionState, MessageSubredditItem
 
-from discord import Embed, Color, Message, TextChannel, Emoji
+import aiostream.stream
+
+from models import Config, SubredditState, SubmissionState
+from time import time, gmtime, strftime
+
+from discord import Embed, Color, Message, TextChannel
 from discord.ext import tasks, commands
 from discord.ext.commands import Bot
 
@@ -12,7 +16,7 @@ from asyncpraw.models import Subreddit
 from asyncpraw.reddit import Submission, Redditor
 from asyncprawcore import Forbidden, NotFound, BadRequest
 
-from colorama import Fore, Back, Style
+from colorama import Fore, Style
 
 
 class RedditCog(commands.Cog, name='RedditCog'):
@@ -21,17 +25,17 @@ class RedditCog(commands.Cog, name='RedditCog'):
         self.reddit: Reddit = reddit
         self.database: Connection = database
         self.config: Config = config
-        self.subreddit: Subreddit = reddit.subreddit(config.reddit_subreddit)
-        self.scrape_scoreboard.start()
-        self.checkup_scoreboard.start()
+        self.find_posts.start()
+        self.update_posts.start()
         self.first_run = True
 
     def cog_unload(self):
-        self.scrape_scoreboard.cancel()
-        self.checkup_scoreboard.cancel()
+        self.find_posts.cancel()
+        self.update_posts.cancel()
 
     @tasks.loop(minutes=15)
-    async def scrape_scoreboard(self):
+    async def find_posts(self):
+        start_time = time()
         channel = self.bot.get_channel(self.config.channel_id)
 
         # Update first run
@@ -41,15 +45,16 @@ class RedditCog(commands.Cog, name='RedditCog'):
         redditrequest: Subreddit = await self.reddit.subreddit('redditrequest')
         await redditrequest.load()
 
+        new_submissions: List[Submission] = await aiostream.stream.list(redditrequest.new(limit=post_limit))
         submission: Submission
-        async for submission in redditrequest.new(limit=post_limit):
+        for submission in reversed(new_submissions):
 
             await submission.load()
             submission_state = await self.get_submission_state(submission)
 
             # Check if post is already in database
             if self.is_already_posted(submission.id):
-                print(f'{Fore.BLUE}{Back.BLACK}    '
+                print(f'{Fore.BLUE}    '
                       f'Submission already posted  '
                       f'{Style.RESET_ALL}')
                 continue
@@ -61,96 +66,91 @@ class RedditCog(commands.Cog, name='RedditCog'):
             subreddit: Subreddit = await self.reddit.subreddit(subreddit_name)
             subreddit_state = await self.get_subreddit_state(subreddit)
 
-            print(f'{Fore.BLUE}{Back.BLACK}    '
-                  f'r/{subreddit_name} - u/{"[deleted]" if author is None else author.name}  '
+            print(f'{Fore.BLUE}    '
+                  f'r/{subreddit_name} - u/{"[deleted]" if author is None else author.name} - '
+                  f'State: {subreddit_state.name}  '
                   f'{Style.RESET_ALL}')
 
             # Build embed, send message, store in database
             embed = await self.build_embed(submission, author, subreddit, subreddit_name, subreddit_state)
             message = await channel.send(embed=embed)
+
             self.put_message_into_database(submission, message)
             self.put_post_into_database(submission, subreddit_name, submission_state)
 
-    @scrape_scoreboard.before_loop
+            # Remove reaction and add reaction if already granted or denied
+            if submission_state == SubmissionState.GRANTED:
+                await message.add_reaction('✔')
+            elif submission_state == SubmissionState.DENIED:
+                await message.add_reaction('❌')
+            else:
+                await message.add_reaction('🆕')
+        stop_time = time()
+        print(f'{Fore.BLUE}> Finished scraping new posts. Took: {strftime("%H:%M:%S", gmtime(stop_time - start_time))}  {Style.RESET_ALL}')
+
+    @find_posts.before_loop
     async def before_scrape_scoreboard(self) -> None:
-        print(f'{Fore.BLUE}{Back.BLACK}> Preparing to scrape new posts {Style.RESET_ALL}')
+        print(f'{Fore.BLUE}> Preparing to scrape new posts  {Style.RESET_ALL}')
         await self.bot.wait_until_ready()
 
     @tasks.loop(hours=2)
-    async def checkup_scoreboard(self):
+    async def update_posts(self):
+        start_time = time()
 
         # Get channel, instantiate the cache list, retrieve the reddit instance
         channel: TextChannel = self.bot.get_channel(self.config.channel_id)
-        revisits: List[MessageSubredditItem] = []
         reddit = self.reddit
 
-        # Get posts to check (only choose )
+        # Get posts to check (only choose posts not younger than min_age or update within min_age or older than max_age)
         cursor = self.database.cursor()
         now = datetime.now()
         min_age: int = int((now - timedelta(hours=self.config.min_post_age)).timestamp())
         max_age: int = int((now - timedelta(days=self.config.max_post_age)).timestamp())
 
         count_stmt = 'SELECT COUNT(*) FROM posts ' \
-                     'WHERE status != ? AND created_at <= ? AND created_at >= ? ' \
+                     'WHERE status != ? AND created_at <= ? AND created_at >= ? AND updated_at <= ? ' \
                      'ORDER BY id'
-        cursor.execute(count_stmt, (SubmissionState.GRANTED.value, min_age, max_age))
+        cursor.execute(count_stmt, (SubmissionState.GRANTED.value, min_age, max_age, min_age))
         estimated_posts = cursor.fetchone()[0]
+        updated_posts = 0
 
+        cursor = self.database.cursor()
         select_stmt = 'SELECT post_id FROM posts ' \
-                      'WHERE status != ? AND created_at <= ? AND created_at >= ? ' \
+                      'WHERE status != ? AND created_at <= ? AND created_at >= ? AND updated_at <= ? ' \
                       'ORDER BY id'
-        cursor.execute(select_stmt, (SubmissionState.GRANTED.value, min_age, max_age))
+        cursor.execute(select_stmt, (SubmissionState.GRANTED.value, min_age, max_age, min_age))
+
+        print(f'{Fore.GREEN}> '
+              f'Revisiting: {Fore.RED}{estimated_posts}{Fore.GREEN} posts with this batch  '
+              f'{Style.RESET_ALL}')
 
         for data in cursor:
-            # Get submission id from database
             submission_id: str = data[0]
-            print(f'{Fore.GREEN}{Back.BLACK}    '
-                  f'Pre-fetching {Fore.RED}{len(revisits)}{Fore.GREEN}/'
-                  f'{Fore.RED}{estimated_posts}{Fore.GREEN} - '
-                  f'{Fore.RED}{int((len(revisits)/estimated_posts) * 100)}%  '
-                  f'{Style.RESET_ALL}')
-            # Get submission from reddit by id
-            submission = await reddit.submission(id=submission_id)
-            try:
-                await submission.load()
-            except Forbidden or NotFound:
-                pass
 
-            # Get message id from database
-            cursor = self.database.cursor()
-            select_stmt = 'SELECT * FROM messages WHERE submission == ?'
-            cursor.execute(select_stmt, (submission_id,))
-            message_id = cursor.fetchone()[1]
+            print(f'{Fore.GREEN}    '
+                  f'Fetching {Fore.RED}{updated_posts}{Fore.GREEN}/'
+                  f'{Fore.RED}{estimated_posts}{Fore.GREEN} - '
+                  f'{Fore.RED}{int(((updated_posts + 1) / estimated_posts) * 100)}%  '
+                  f'{Style.RESET_ALL}')
+
+            submission = await reddit.submission(id=submission_id)
+            message_id: int = self.get_message_id_from_database(submission_id)
 
             # Get message from discord
             message: Message = await channel.fetch_message(message_id)
-            await message.add_reaction('🔜')
+            await message.remove_reaction('🆕', self.bot.user)
 
-            # Store in list
-            revisits.append(MessageSubredditItem(submission_id, submission, message_id, message))
-
-        print(f'{Fore.GREEN}{Back.BLACK}> '
-              f'Revisiting: {Fore.RED}{len(revisits)}{Fore.GREEN} posts with this batch  '
-              f'{Style.RESET_ALL}')
-
-        for item in revisits:
             # Add loading reaction-emoji to message
-            await item.message.remove_reaction('🔜', self.bot.user)
-            await item.message.add_reaction('🔄')
+            await message.add_reaction('🔄')
 
             # get message and submission
-            subreddit_name = self.get_subreddit_name_from_url(item.submission.url)
+            subreddit_name = self.get_subreddit_name_from_url(submission.url)
             subreddit = await reddit.subreddit(subreddit_name)
             subreddit_state = await self.get_subreddit_state(subreddit)
 
-            submission = item.submission
+            submission = submission
             author = submission.author
             await author.load()
-
-            # Update on CLI
-            print(f'{Fore.GREEN}{Back.BLACK}    '
-                  f'{item.submission_id}: {subreddit_name} '
-                  f'{Style.RESET_ALL}')
 
             # Update embed in Discord message
             embed = await self.build_embed(submission,
@@ -158,28 +158,47 @@ class RedditCog(commands.Cog, name='RedditCog'):
                                            subreddit,
                                            subreddit_name,
                                            subreddit_state)
-            await item.message.edit(embed=embed)
+            await message.edit(embed=embed)
 
             # Prepare database update
             timestamp: int = int(datetime.now().timestamp())
-            submission_state: SubmissionState = await self.get_submission_state(item.submission)
-            submission_id: str = item.submission_id
-            message_id: int = item.message_id
+            submission_state: SubmissionState = await self.get_submission_state(submission)
+            submission_id: str = submission_id
+            message_id: int = message_id
 
+            # Update on CLI
+            print(f'    {Fore.RED}{updated_posts}{Fore.GREEN}/'
+                  f'{Fore.RED}{estimated_posts}{Fore.GREEN} - '
+                  f'{Fore.RED}{int(((updated_posts + 1) / estimated_posts) * 100)}{Fore.GREEN}% '
+                  f'{submission_id}: {subreddit_name} state: {submission_state.name} '
+                  f'{Style.RESET_ALL}')
+
+            # Update database
             posts_update_stmt = 'UPDATE posts SET updated_at = ?, status = ? WHERE post_id == ?'
-            messages_update_stmt = "UPDATE messages SET updated_at = ? WHERE message_id == ?"
+            messages_update_stmt = 'UPDATE messages SET updated_at = ? WHERE message_id == ?'
 
             # Update timestamps in database
             cursor.execute(posts_update_stmt, (timestamp, submission_state.value, submission_id))
             cursor.execute(messages_update_stmt, (timestamp, message_id))
             self.database.commit()
 
-            # Remove reaction
-            await item.message.remove_reaction('🔄', self.bot.user)
+            # Remove reaction and add reaction if already granted or denied
+            await message.remove_reaction('🔄', self.bot.user)
+            if submission_state == SubmissionState.GRANTED:
+                await message.add_reaction('✔')
+            elif submission_state == SubmissionState.DENIED:
+                await message.add_reaction('❌')
+            updated_posts += 1
 
-    @checkup_scoreboard.before_loop
+        stop_time = time()
+        print(f'{Fore.GREEN}> '
+              f'Finished: Revisited {Fore.RED}{updated_posts}{Fore.GREEN} posts with this batch '
+              f'{strftime("%H:%M:%S", gmtime(stop_time - start_time))}s  '
+              f'{Style.RESET_ALL}')
+
+    @update_posts.before_loop
     async def before_checkup_scoreboard(self) -> None:
-        print(f'{Fore.GREEN}{Back.BLACK}> Getting ready to validate previous posts {Style.RESET_ALL}')
+        print(f'{Fore.GREEN}> Getting ready to validate previous posts {Style.RESET_ALL}')
         await self.bot.wait_until_ready()
 
     def get_subreddit_name_from_url(self, url: str) -> str:
@@ -203,6 +222,12 @@ class RedditCog(commands.Cog, name='RedditCog'):
         if len(data) == 0:
             return False
         return True
+
+    def get_message_id_from_database(self, submission_id: str):
+        cursor = self.database.cursor()
+        select_stmt = 'SELECT * FROM messages WHERE submission == ?'
+        cursor.execute(select_stmt, (submission_id, ))
+        return cursor.fetchone()[1]
 
     def put_post_into_database(self, submission: Submission, subreddit_name: str, submission_state: SubmissionState) \
             -> None:
@@ -298,8 +323,12 @@ class RedditCog(commands.Cog, name='RedditCog'):
             moderators.append(f'u/{mod.name}')
         return moderators
 
-    async def build_embed(self, submission: Submission, author: Redditor, subreddit: Subreddit, subreddit_name: str, subreddit_state: SubredditState) -> Embed:
-        state = await self.get_subreddit_state(subreddit)
+    async def build_embed(self, submission: Submission,
+                          author: Redditor,
+                          subreddit: Subreddit,
+                          subreddit_name: str,
+                          subreddit_state: SubredditState) -> Embed:
+        state = subreddit_state
         submission_state = await self.get_submission_state(submission)
 
         embed = Embed(title=f'r/{subreddit_name}', color=self.get_embed_color(submission_state))
